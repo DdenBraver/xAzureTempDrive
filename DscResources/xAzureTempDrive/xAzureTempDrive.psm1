f9cc7c5ab5ad1a3a8b74693abf4de95416f4477c
Danny den Braver <danny@denbraver.com>
2017-06-06 21:39:03 +0200

    first commit - version 1.0.0.0

---
+++ DscResources/xAzureTempDrive/xAzureTempDrive.psm1
@@ -0,0 +1,74 @@
+﻿function Get-TargetResource 
+{    
+    [OutputType([System.Collections.Hashtable])] 
+  param 
+   (   
+	[Parameter(Mandatory)] 
+        [string]$DriveLetter
+
+   ) 
+   
+    $pagefile = gwmi win32_pagefilesetting
+    if ($pagefile -ne $null) {
+        $pagefile = $pagefile.Name.ToLower()  
+    }
+
+    $returnValue = @{ 
+        DriveLetter = $pagefile      
+    }
+         
+    $returnValue 
+} 
+
+function Set-TargetResource 
+{   
+   param 
+    ( 
+      [Parameter(Mandatory)] 
+        [string]$DriveLetter   
+    ) 
+    
+    $CurrentValue = (Get-TargetResource @PSBoundParameters).DriveLetter
+
+    if ($currentvalue -ne $null) {
+        $pagefile = gwmi win32_pagefilesetting
+        $pagefile.Delete()
+        $global:DSCMachineStatus = 1 
+    }
+
+    else {
+        $CurrentDriveLetter = (get-volume -FileSystemLabel "Temporary Storage").DriveLetter
+        write-verbose "Changing drive letter from [$CurrentDriveLetter] to [$DriveLetter]"
+        Get-Partition -DriveLetter $CurrentDriveLetter | Set-Partition -NewDriveLetter $DriveLetter
+        
+        $DriveLetter = $DriveLetter + ":"
+        $drive = Get-WmiObject -Class win32_volume -Filter “DriveLetter = '$DriveLetter'”
+        write-verbose "Attaching pagefile to the new Driveletter"
+        Set-WMIInstance -Class Win32_PageFileSetting -Arguments @{ Name = "$DriveLetter\pagefile.sys"; MaximumSize = 0; }
+
+        $global:DSCMachineStatus = 1 
+    }             
+    
+} 
+
+function Test-TargetResource 
+{ 
+    [OutputType([System.Boolean])]    
+    param 
+     ( 
+      	[Parameter(Mandatory)] 
+        [string]$DriveLetter
+    ) 
+    
+    $CurrentValue = Get-TargetResource @PSBoundParameters
+
+    if ($CurrentValue.DriveLetter -like "$DriveLetter*") {
+        return $true
+    }
+    else {
+        return $false
+    }
+} 
+
+
+Export-ModuleMember -Function *-TargetResource 
---
+++ readme.md
@@ -0,0 +1,43 @@
+# xAzureTempDrive
+
+The **xAzureTempDrive** DSC resource lets you modify your Azure Temporary Drive
+
+## Contributing
+Please check out common DSC Resources [contributing guidelines](https://github.com/PowerShell/DscResource.Kit/blob/master/CONTRIBUTING.md).
+
+## Resources
+
+* **xAzureTempDrive** Makes you change the temporary driveletter while maintaining its pagefile.
+
+### xAzureTempDrive
+
+* **Driveletter**: Driveletter that should be used for the Azure Temporary Drive
+
+## Versions
+
+### 1.0.0.0
+
+* First version of the module
+
+
+## Examples
+
+### Configuring a new drive letter for the Azure Temporary Drive
+
+```powershell
+configuration Sample_xAzureTempDrive
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]$Driveletter
+    )
+    Import-DscResource -module xAzureTempDrive
+    
+	xAzureTempDrive AzureTempDrive
+    {
+        Driveletter = $Driveletter
+    }
+}
+Sample_Sample_xAzureTempDrive -Driveletter 'Z'
+```
